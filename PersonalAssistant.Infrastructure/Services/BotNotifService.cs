8b300646064320a5e11755ce5ac677a9cf17400c
MrSal0 <volody4859@gmail.com>
2026-09-24 07:26:38 +0300

    feat: added edit message method, added markup on send message (requires separation on middleware)

--- PersonalAssistant.Infrastructure/Services/BotNotifService.cs
+++ PersonalAssistant.Infrastructure/Services/BotNotifService.cs
@@ -1,5 +1,9 @@
 ﻿using Telegram.Bot;
 using PersonalAssistant.Application.Interfaces;
+using Telegram.Bot.Types.ReplyMarkups;
+using Microsoft.Extensions.Logging;
+using PersonalAssistant.Infrastructure.Workers;
+using Telegram.Bot.Exceptions;
 
 
 namespace PersonalAssistant.Infrastructure.Services
@@ -7,15 +11,43 @@ namespace PersonalAssistant.Infrastructure.Services
     public class BotNotifService: IBotNotifService
     {
         private readonly ITelegramBotClient _botClient;
+        private readonly ILogger<BotNotifService> _logger;
 
-        public BotNotifService(ITelegramBotClient botClient)
+        public BotNotifService(ITelegramBotClient botClient, ILogger<BotNotifService> logger)
         {
             _botClient = botClient;
+            _logger = logger;
         }
 
-        public async Task SendMessageAsync(long chatId, string text, CancellationToken cancellationToken = default)
+        public async Task SendMessageAsync(long chatId, string text, InlineKeyboardMarkup? replyMarkup = null, CancellationToken cancellationToken = default)
         {
-            await _botClient.SendMessage(chatId, text, cancellationToken: cancellationToken);
+            await _botClient.SendMessage(chatId, text, cancellationToken: cancellationToken, replyMarkup: replyMarkup);
+        }
+
+        public async Task EditMessageAsync(long chatId, int messageId, string text, InlineKeyboardMarkup? replyMarkup = null, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                await _botClient.EditMessageText(
+                    chatId: chatId,
+                    messageId: messageId,
+                    text: text,
+                    replyMarkup: replyMarkup,
+                    cancellationToken: cancellationToken
+                );
+            }
+            catch (ApiRequestException) 
+            {
+                await SendMessageAsync(
+                    chatId: chatId,
+                    text: text,
+                    replyMarkup: replyMarkup,
+                    cancellationToken: cancellationToken);
+            }
+            catch(Exception ex) {
+                _logger.LogWarning($"Catched an {ex}");
+            }
+
         }
 
     }
